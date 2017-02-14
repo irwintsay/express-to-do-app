7dcea798f6d2b6c8888affa94c0458cf9ebf5303
Irwin Tsay <irwintsay@gmail.com>
2017-02-14 12:28:46 -0500

    Update public assets

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app         = express();
 const PORT        = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./resources'));
+
 app.listen(PORT, () => { console.log('Listening on', PORT)});
